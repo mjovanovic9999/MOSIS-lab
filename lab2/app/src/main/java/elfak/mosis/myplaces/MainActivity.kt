b1bd5f5657ef4b76fec914932055e02b76a58b22
Milos <m.jovanovic@elfak.rs>
2022-05-11 21:52:58 +0200

    nasstavak lab2

--- lab2/app/src/main/java/elfak/mosis/myplaces/MainActivity.kt
+++ lab2/app/src/main/java/elfak/mosis/myplaces/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import elfak.mosis.myplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -44,10 +45,13 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.action_show_map -> Toast.makeText(this,"Show Map!",Toast.LENGTH_SHORT).show()
+            R.id.action_new_place -> Toast.makeText(this,"New Place!!",Toast.LENGTH_SHORT).show()
+            R.id.action_my_places_list -> Toast.makeText(this,"My Places List!",Toast.LENGTH_SHORT).show()
+            R.id.action_about -> Toast.makeText(this,"About!",Toast.LENGTH_SHORT).show()
         }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
